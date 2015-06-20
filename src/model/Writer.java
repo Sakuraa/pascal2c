5f276a914ad6c4898512e89e0f42da99f111c450
unknown <martyna.jablonska7@gmail.com>
2015-06-20 13:31:09 +0200

    Dodany program name

--- src/model/Writer.java
+++ src/model/Writer.java
@@ -1,7 +1,10 @@
 package model;
 
+import java.io.ObjectInputStream.GetField;
+
 public class Writer {
 	private StringBuilder code;
+	private String programName;
 	public Writer() {
 		// TODO Auto-generated constructor stub
 		code = new StringBuilder();
@@ -11,5 +14,11 @@ public class Writer {
 	}
 	public StringBuilder getCode() {
 		return code;
+	}
+	public String getProgramName() {
+		return programName;
+	}
+	public void setProgramName(String programName) {
+		this.programName = programName;
 	}
 }
